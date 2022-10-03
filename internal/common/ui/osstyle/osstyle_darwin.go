693a321537b1149eb2201a0a9589082023fdaf6f
Leon Mika <lmika@lmika.org>
2022-10-04 10:05:07 +1100

    Fixed bug which was not properly detecting whether macOS is in light mode

--- internal/common/ui/osstyle/osstyle_darwin.go
+++ internal/common/ui/osstyle/osstyle_darwin.go
@@ -1,15 +1,34 @@
 package osstyle
 
 import (
+	"github.com/pkg/errors"
 	"log"
 	"os/exec"
+	"strings"
+)
+
+const (
+	errorMessageIndicatingInLightMode = `The domain/default pair of (kCFPreferencesAnyApplication, AppleInterfaceStyle) does not exist`
 )
 
 // Usage: https://stefan.sofa-rockers.org/2018/10/23/macos-dark-mode-terminal-vim/
 func darwinGetOSColorScheme() ColorScheme {
 	d, err := exec.Command("defaults", "read", "-g", "AppleInterfaceStyle").Output()
 	if err != nil {
-		log.Printf("cannot get current OS color scheme: %v", err)
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			stdErr := string(exitErr.Stderr)
+
+			if strings.Contains(stdErr, errorMessageIndicatingInLightMode) {
+				log.Printf("error message indicates that macOS is in light mode")
+				return ColorSchemeLightMode
+			}
+
+			log.Printf("cannot get current OS color scheme: %v - stderr: [%v]", err, stdErr)
+		} else {
+			log.Printf("cannot get current OS color scheme: %v", err)
+		}
+
 		return ColorSchemeUnknown
 	}
 
