12ea1a0c665c67b22f11cf0c95b4f1572c8f0593
Leon Mika <lmika@lmika.org>
2023-10-01 20:51:28 +0000

    Fixed failing unit tests

--- internal/dynamo-browse/providers/pasteboardprovider/nilprovider.go
+++ internal/dynamo-browse/providers/pasteboardprovider/nilprovider.go
@@ -2,6 +2,10 @@ package pasteboardprovider
 
 type NilProvider struct{}
 
+func (NilProvider) ReadText() (string, bool) {
+	return "", false
+}
+
 func (n NilProvider) WriteText(bts []byte) error {
 	return nil
 }
